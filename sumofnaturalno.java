3ca4ffe42601b0ace3d0115fc26fbae7b1aed132
Nikhil Aitipamula <91412906+nikhilaitipamula@users.noreply.github.com>
2022-09-24 09:27:34 +0530

    Add files via upload

---
+++ sumofnaturalno.java
@@ -0,0 +1,12 @@
+public class sumofnaturalno {
+
+          public static void main(String args[])
+          {
+                    int r=0;
+                    for(int i=1;i<=50;i++)
+                    {
+                              r=r+i;
+                    }   
+                    System.out.println("The sum of all natural numbers 1 to 50 is : "+r);
+          }
+}
